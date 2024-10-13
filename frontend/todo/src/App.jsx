9f3d3ff93cedda1eb935198e35093300f5cb7659
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2024-10-13 22:05:32 +0530

    update the update todo

--- frontend/todo/src/App.jsx
+++ frontend/todo/src/App.jsx
@@ -16,7 +16,6 @@ function App() {
   // this function is for giving the title and desc
   function handlesubmit() {
     setError("");
-    console.log("working");
     if (title.trim !== '' || desc.trim !== '') {
       // api calling
       fetch(apiUrl + "todos", {
@@ -59,7 +58,42 @@ function App() {
     getItems()
   }, [])
 
-
+  const handleUpdate = () => {
+    if (editTitle.trim !== '' || editDesc.trim !== ''){
+      fetch(apiUrl+"todos/"+editId,{
+        method: "PUT",
+        headers:{
+          "Content-Type" : "application/json"
+        },
+        body:JSON.stringify({title : editTitle,desc : editDesc})
+      }).then((res) => {
+        if(res.ok){
+          // update the items to list 
+          const updateTodos = todos.map((items) => {
+            if(items._id == editId){
+              items.title = editTitle;
+              items.desc = editDesc;
+            }
+            return items
+          })
+          setTodos(updateTodos);
+          setSuccess("Item update successfully")
+          setTimeout(() => {
+            setSuccess("");
+          },3000)
+          setEditId(-1);
+        }
+      })
+    }
+  }
+  const handleEdit = (items) => {
+    setEditId(items._id);
+    setEditTitle(items.title);
+    setEditDesc(items.desc);
+  }
+  const handleCancelEdit = () => {
+    setEditId(-1); 
+  }
   return (
     <>
       <div className="navbar-container">
@@ -84,7 +118,7 @@ function App() {
                 <li>
                   <div className="tasks-content">
                     {
-                      editId !== -1
+                      editId == -1 || editId !== items._id
                         ?
                         <>
                           <span className="title-content">{items.title}</span>
@@ -92,15 +126,27 @@ function App() {
                         </>
                         :
                         <>
-                          <input className="input-title" type="text" onChange={(e) => setEditTitle(e.target.value)} value={title} name="username" id="username" placeholder="Add title.." />
-                          <input className="input-desc" type="text" onChange={(e) => setEditDesc(e.target.value)} value={desc} name="password" id="password" placeholder="Add description.." />
+                          <input className="input-title" type="text" onChange={(e) => setEditTitle(e.target.value)} value={editTitle} name="username" id="username" placeholder="Add title.." />
+                          <input className="input-desc" type="text" onChange={(e) => setEditDesc(e.target.value)} value={editDesc} name="password" id="password" placeholder="Add description.." />
                         </>
                     }
 
 
                     <div className="tasks-btns">
-                      <i class="fa-regular fa-pen-to-square"></i>
-                      <i onClick={() => setEditId(items._id)} class="fa-solid fa-trash trash"></i>
+                     
+                      {
+                        editId == -1 || editId !== items._id
+                        ?
+                        <>
+                          <i onClick={() => handleEdit(items)} class="fa-regular fa-pen-to-square"></i>
+                          <i class="fa-solid fa-trash trash"></i>
+                        </>
+                        :
+                        <>
+                          <i class="fa-solid fa-xmark" onClick={handleCancelEdit}></i>
+                          <i class="fa-regular fa-floppy-disk fa-pen-to-square" onClick={handleUpdate}></i>
+                        </>
+                      }
                     </div>
                   </div>
                 </li>)
